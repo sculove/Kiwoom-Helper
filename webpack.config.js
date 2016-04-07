c2c264a98b4a6f6b094ad941e6fc3f20990e8f0c
sculove <sculove@gmail.com>
2016-04-08 00:31:22 +0900

    production 모드 처리

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
+var NODE_ENV = process.env.NODE_ENV || 'development';
 var webpack = require("webpack");
-module.exports = {
+var config = {
 	context: __dirname + '/src',
 	entry : {
 		build : "./kiwoom-helper.js"
 	},
 	output: {
-		filename : "kiwoom-helper.js",
+		filename : NODE_ENV === "production" ? "kiwoom-helper.min.js" : "kiwoom-helper.js",
 		path: __dirname + "/dist"
 	},
-	devtool: "source-map",
+	devtool: NODE_ENV === "production" ? "source-map" : "inline-source-map",
 	module: {
 		loaders: [
 			{
@@ -22,5 +23,20 @@ module.exports = {
 				}
 			}
 		]
-	}
+	},
+	plugins: [
+	]
 };
+
+if (NODE_ENV === 'production') {
+	config.plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+		  compress: {
+		    warnings:     false,
+		    drop_console: true
+		  },
+		})
+	);
+}
+
+module.exports = config;
